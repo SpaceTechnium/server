cc0ce2101129b4c7b8de3589b8322478f0b5ecfc
Victor Carvalho <contato@victorcarvalho.pt>
2018-05-17 16:41:34 +0100

    Added files for server to implement collision

---
+++ game_server/ship.js
@@ -0,0 +1,21 @@
+//================================================
+// Ship.js
+//================================================
+class Ship {
+    constructor() {
+        // Attributes
+        this.pos_x = 0;
+        this.pos_y = 0;
+        this.pos_z = 0;
+        this.rot_x = 0;
+        this.rot_y = 0;
+        this.rot_z = 0;
+        this.score = 0;
+        this.oscilation = 0;
+        this.bullets    = [];
+    }
+
+    update(shipVector) {
+        //TODO: implement
+    }
+}
