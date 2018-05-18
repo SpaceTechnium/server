f50cf6ee74f12ba03c013e64e4fc94e64fd98ba8
Victor Carvalho <tocco@artemis.lan>
2018-05-18 21:06:50 +0100

    Finalizing touches for gameserver

--- game_server/player.js
+++ game_server/player.js
@@ -7,11 +7,19 @@ class Vector3 {
 }
 
 const BULLET_BOUNDING_BOX = 0;
+const BULLET_SPEED = 1;
 
 class Bullet {
-    constructor(pos) {
-        this.pos = pos;
+    constructor(pos_v3, rot_v3) {
+        this.pos = pos_v3;
+        this.rot = rot_v3;
         this.boundingBox = BULLET_BOUNDING_BOX;
+    }
+
+    update () {
+        this.pos.x += (this.rot.x * speed);
+        this.pos.y += (this.rot.y * speed);
+        this.pos.z += (this.rot.z * speed);
     }
 }
 
@@ -32,9 +40,19 @@ class Player {
         this.bullets = [];
         this.boundingBox = PLAYER_BOUNDING_BOX;
     }
+
+    update(pos, rot) {
+        this.pos_x = pos.x;
+        this.pos_y = pos.y;
+        this.pos_z = pos.z;
+        this.rot_x = rot.x;
+        this.rot_y = rot.y;
+        this.rot_z = rot.z;
+    }
 }
 
 module.exports = {
-    Player,
-    Bullet
-}
+    Vector3,
+    Bullet,
+    Player
+};
